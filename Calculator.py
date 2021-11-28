3fb32bef958b51120d176605efadcac27f4c1fb8
Henrik <hg04@web.de>
2021-11-28 12:36:47 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,38 +1,33 @@
 import time
 
+first_question = input("Hello and welcome to the Calculator write the sign (*, /, +, - ONLY):   ")
 
-
-
-first_question = input("Hello and welcome to the Calculator write the sign (*, /, +, - ONLY) now:   ")
-
+error1 = "Not correct! you cannot take a number / 0"
 
 if first_question == "*":
     number_one = int(input("Write the first number. ➡  "))
     number_two = int(input("Write the Second number. ➡  "))
     answer = number_one * number_two
-    print(number_one * number_two)
+    print(answer)
 
 
 if first_question == "/":
     number_one = int(input("Write the first number. ➡ "))
     number_two = int(input("Write the Second number. ➡ "))
-    answer = number_one * number_two
-    print(number_one * number_two)
+    if number_two != 0:
+        answer = number_one / number_two
+        print(answer)
+    else:
+        print(error1)
 
 if first_question == "+":
     number_one = int(input("Write the first number. ➡ "))
     number_two = int(input("Write the Second number. ➡ "))
-    answer = number_one * number_two
-    print(number_one * number_two)
+    answer = number_one + number_two
+    print(answer)
 
 if first_question == "-":
     number_one = int(input("Write the first number. ➡ "))
     number_two = int(input("Write the Second number. ➡ "))
-    answer = number_one * number_two
-    print(number_one * number_two)
-
-
-error1 = "Not correct! you cannot take " + str(number_one) + " / 0"
-if first_question == "/":
-    if number_two == 0:
-        print(error1)
+    answer = number_one - number_two
+    print(answer)
